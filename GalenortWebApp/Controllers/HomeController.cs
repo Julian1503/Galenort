d1c5dfa16399fac7a1bdbb5843534acea75de8de
Selena García <sgarcia.azt@gmail.com>
2020-07-28 00:44:04 -0300

    primera fase de cenro de atencion y home

--- GalenortWebApp/Controllers/HomeController.cs
+++ GalenortWebApp/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using GalenortWebApp.Models.CentroAtencion;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -12,19 +14,35 @@ namespace GalenortWebApp.Controllers
         {
             return View();
         }
-
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
-        public ActionResult CentroAtencion()
-        {
-            ViewBag.Message = "Your contact page.";
+        //public async Task<ActionResult> CentroAtencion(string prestador, int? page, string cadenaBuscar, string establecimientoBuscar)
+        //{
+        //    var pageNumber = (page ?? 1);
 
-            return View();
-        }
+        //    ViewBag.FilterValue = cadenaBuscar;
+
+        //   var _prestador = await _prestadorServicio.Obtener(prestador, cadenaBuscar, establecimientoBuscar);
+
+        //    return View(new CentroAtencionViewModel
+        //    {
+               
+        //        Centros = await _prestador
+        //            .OrderByDescending(x => x.FechaDesde)
+        //            .Select(s => new CentroAtencionViewModel
+        //            {
+        //                CentroAtencionId = s.Id,
+        //                Prestador = s.Prestador,
+        //                Horario = s.Horario.ToShortDateString(),
+        //                Dias = s.Dias.ToShortDateString(),
+        //                Establecimiento = s.Establecimiento,
+        //            }).ToPagedListAsync(pageNumber, 10)
+        //    });
+        //}
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
@@ -37,5 +55,11 @@ namespace GalenortWebApp.Controllers
 
             return View();
         }
+        public ActionResult Afiliado()
+        {
+
+            return View();
+        }
+
     }
 }
---
+++ GalenortWebApp/Models/CentroAtencion/CentroAtencionViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GalenortWebApp.Models.CentroAtencion
+{
+    public class CentroAtencionViewModel
+
+    {
+        public long CentroAtencionId { get; set; }
+
+        public string Horario { get; set; }
+
+        public string Dias { get; set; }
+
+        public string Establecimiento { get; set; }
+
+        public string Prestador { get; set; }
+
+        public int? Page { get; set; }
+
+        public string CadenaBuscar { get; set; }
+        public string EspecialidadBuscar { get; set; }
+
+        public IEnumerable<CentroAtencionViewModel> Centros { get; set; }
+
+    }
+}
